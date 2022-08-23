b3149a6fd4e8f5ce1eed30b0f2bf1728c39e7ff4
faraz3 <farazyanuar1@gmail.com>
2022-08-23 19:48:47 +0700

    buat bagian personal

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+// import 'bootstrap/'
+// import 'bootstrap/dist/css/bootstrap-icon.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Menu from '@/components/Menu.vue'
+</script>
+
+<template>
+  <main>
+    <Menu />
+  </main>
+</template>
